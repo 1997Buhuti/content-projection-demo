622bfc1c8442bdf9af82ba2ade34c79645de5a1c
Buhuti Manakal <manakal@randomsoftware.net>
2024-02-22 21:21:06 +0530

    init repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { AuthFormComponent } from './auth-form/auth-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    FormsModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatIconModule,
+    AuthFormComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
