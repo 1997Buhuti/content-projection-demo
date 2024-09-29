b2850f0901fcc200076fb0c34a8c628b6355926d
manakal <manakal@randomsoftware.net>
2024-09-29 17:00:25 +0530

    Added Initial Example

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -3,32 +3,71 @@ import {
   Component,
   ContentChild,
   EventEmitter,
+  Input,
+  OnInit,
   Output,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotifyMeComponent } from '../notify-me/notify-me.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule, NotifyMeComponent],
+  imports: [
+    CommonModule,
+    NotifyMeComponent,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatRadioModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
 })
-export class ProductCardComponent implements AfterContentInit {
+export class ProductCardComponent implements OnInit {
   @Output() isAddToCartClicked = new EventEmitter();
   @ContentChild(NotifyMeComponent) isNotifyMeChecked!: NotifyMeComponent;
-  showMessage: boolean = false;
+  @Input() productName: string = 'Mountain Bicycle';
+  @Input() productDescription: string =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
 
-  ngAfterContentInit() {
-    if (this.isNotifyMeChecked) {
-      console.log(this.isNotifyMeChecked);
-      this.isNotifyMeChecked.checked.subscribe((value) => {
-        value? this.showMessage = true : this.showMessage=false;
-      });
+  @Input() productType: ProductType = ProductType.BIKE;
+  showMessage: boolean = false;
+  imageUri: string = '';
+
+  ngOnInit(): void {
+    this.setImageURI(this.productType);
+  }
+  setImageURI(productType: ProductType) {
+    switch (productType) {
+      case ProductType.BIKE:
+        this.imageUri = '../assets/22.jpg';
+        break;
+      case ProductType.COCA_COLA:
+        this.imageUri = '../assets/coca-cola.jpg';
+        break;
+      case ProductType.FRUIT:
+        this.imageUri = '../assets/pineapple.jpg';
+        break;
     }
   }
   addToCart() {
     this.isAddToCartClicked.emit(true);
   }
+}
+
+export enum ProductType {
+  COCA_COLA = 'COCA_COLA',
+  FRUIT = 'FRUIT',
+  BIKE = 'BIKE',
 }
